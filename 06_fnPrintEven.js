a59a866212c189a2f3d84b7938e2eb81b48e5c6e
dinruz <210591018+dinruz@users.noreply.github.com>
2025-07-25 02:30:25 +0200

    feat: Add function exercises 05 & 06 - Add exercise 05 - function Add - Add exercise 06 - function Print Even Numbers

---
+++ 06_fnPrintEven.js
@@ -0,0 +1,8 @@
+function printEvenNumbers(start, end) {
+    for (let i = start; i <= end; i++) {
+        if (i % 2 === 0) { 
+             console.log(`${i}`);
+        }
+  }
+}
+printEvenNumbers(1, 15);
