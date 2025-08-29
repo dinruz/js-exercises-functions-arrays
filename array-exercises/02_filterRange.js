f31d7f8f4214a4f94d92d3f5b9936445c44bc026
dinruz <210591018+dinruz@users.noreply.github.com>
2025-08-29 20:37:05 +0200

    feat: Add second solution with arrow func - arr.ex.02

--- array-exercises/02_filterRange.js
+++ array-exercises/02_filterRange.js
@@ -1,22 +1,28 @@
+// Initial Data
 
 let arr = [5, 3, 8, 1];
+console.log("Original array of numbers: ",arr)
 
-console.log("Filtering elements");
+// 1 - if/else
 
 function filterRange(arr,a, b) {
+    console.log("Filtering elements with if-else statements:");
     return arr.filter(function(element) { 
-       
-        if ((element >= a) && (element <= b)) {
+               if ((element >= a) && (element <= b)) {
             return true;
         } else {
             return false; 
         }
     });
-
 }
 filtered=filterRange(arr, 1, 4);
+console.log(filtered); 
 
-console.log("Original array: ",arr)
-console.log("Filtered array: ",filtered); 
+//2 - arrow func
 
-
+function filterRange2 (arr,a,b){
+    console.log("Filtering elements with arrow function:");
+     let filtered2 = arr.filter((num)=> (num>=a)&&(num<=b))
+    console.log(filtered2);
+}
+filterRange2(arr,1,4);
